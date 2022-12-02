b8ac441a184be1d9ae5489db93af5507d908f6f6
framerage <officialigonin@mail.ru>
2022-12-02 18:56:21 +0400

    'try to use mvc in header'

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,41 +1,27 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { changeUserTheme } from "store/personTheme/actions";
-import { selectUserTheme } from "store/personTheme/selectors";
-import { AppDispatch } from "types/appTypes";
-import { THEMES_COLORS } from "utils/constances/constances";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {changeUserTheme} from "store/personTheme/actions";
+import {selectUserTheme} from "store/personTheme/selectors";
+import {AppDispatch} from "types/appTypes";
+import {THEMES_COLORS} from "utils/constances/constances";
 import "./header.scss";
-const Header = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
-  const userTheme = useSelector(selectUserTheme);
-  const navToHome = () => {
-    navigate("/");
-  };
-
-  useEffect(() => {
-    document.documentElement.style.setProperty("--themeColor", `${userTheme}`);
-  }, [userTheme]);
-
-  const changeTheme = (choosedColor: string) => {
-    document.documentElement.style.setProperty(
-      "--themeColor",
-      `${choosedColor}`
-    );
-    dispatch(changeUserTheme(choosedColor));
-  };
+type HeaderProps = {
+  onChangeTheme: Function;
+  navToHome: () => void;
+};
+const Header = ({navToHome, onChangeTheme}: HeaderProps) => {
   return (
     <div className="header">
       <div className="header__logo" onClick={navToHome}></div>
       <div className="header__outline">
-        {THEMES_COLORS.map((color) => (
+        {THEMES_COLORS.map(color => (
           <div
             key={color}
             role="presentation"
             className="themeColorBlock"
-            style={{ backgroundColor: `${color}` }}
-            onClick={() => changeTheme(color)}
+            style={{backgroundColor: `${color}`}}
+            onClick={() => onChangeTheme(color)}
           ></div>
         ))}
       </div>
--- src/components/Layuot/Layout.tsx
+++ src/components/Layuot/Layout.tsx
@@ -1,13 +1,13 @@
-import Header from "components/Header/Header";
+import HeaderControl from "components/Header/HeaderControl";
 import DescripPage from "pages/Description/DescripPage";
 import Home from "pages/Home/Home";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import "./layout.scss";
 const Layout = () => {
   return (
     <div className="wrapper">
-      <Header />
+      <HeaderControl />
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/:id" element={<DescripPage />} />
